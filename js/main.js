8a2f053c7267394df1a70c7c87160b1ffc64dbf2
Tan Huy H. Nguyen <tanhuy@me.com>
2018-09-20 11:41:00 +0900

    Project boxes nearly completed. Would like to add overlay information at some point.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
 // LANDING
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
 
-left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
 });
 
-left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
 });
 
-right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
 });
 
-right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
 });
 
 // SMOOTH SCROLL
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
- 
-for (let item of anchorlinks) { // relitere 
-    item.addEventListener('click', (e)=> {
-        let hashval = item.getAttribute('href')
-        let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
-    })
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
+
+for (let item of anchorlinks) {
+  // relitere
+  item.addEventListener("click", e => {
+    let x = item.getAttribute("href");
+    let target = document.querySelector(x);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+    history.pushState(null, null, x);
+    e.preventDefault();
+  });
 }
