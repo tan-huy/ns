2578659ee7ee1696bb61b21e484011f4b7fb911f
Tan Huy H. Nguyen <tanhuy@me.com>
2018-10-05 14:33:03 +0900

    Form inputs added. CSS needs more work.

--- js/main.js
+++ js/main.js
@@ -18,3 +18,20 @@ right.addEventListener("mouseenter", () => {
 right.addEventListener("mouseleave", () => {
   container.classList.remove("hover-right");
 });
+
+// FORM COUNTDOWN
+function limitTextCount(limitField_id, limitCount_id, limitNum)
+{
+    var limitField = document.getElementById(limitField_id);
+    var limitCount = document.getElementById(limitCount_id);
+    var fieldLEN = limitField.value.length;
+
+    if (fieldLEN > limitNum)
+    {
+        limitField.value = limitField.value.substring(0, limitNum);
+    }
+    else
+    {
+        limitCount.innerHTML = (limitNum - fieldLEN) + '/500';
+    }
+}
